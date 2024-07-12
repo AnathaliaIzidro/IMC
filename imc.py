e9519535d5d1ea66b0edddf6e69a3d46ac205a3d
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 12:08:00 -0300

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,13 @@
+# 3. Crie uma variável e armazene o seu peso e outra  armazen sua altura. Em seguida crie uma variável para armazenar o imc e exiba usando o comando print
+# imc = peso / altura²
+
+# peso = 82
+# altura = 1.81
+# imc = peso / altura ** 2
+# print(f"Imc = {imc:.2f}")
+
+peso = 69
+altura = 1.62
+
+imc = peso / (altura ** 2)
+print(f'Valor do imc: {imc:.2f}')
